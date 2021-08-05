a9dc4757473ca4a76f0a9dad25ded3208d15a7cc
alexandru.castravet <kacmpa@gmail.com>
2021-08-05 23:02:39 +0300

    add all code from capitol

--- src/main/scala/chapter_1/string/TestingStringEquality_1_1.scala
+++ src/main/scala/chapter_1/string/TestingStringEquality_1_1.scala
@@ -1,6 +1,25 @@
 package chapter_1.string
 
 object TestingStringEquality_1_1 extends App {
+  val s1 = "Hello"
+  val s2 = "Hello"
+  val s3 = "H" + "ello"
+  println(s1 == s2)
+  println(s1 == s3)
 
+  val s4: String = null
+  println(s3 == s4)
+  println(s4 == s3)
 
+  val a1 = "Hello"
+  val a2 = "hello"
+  println(s1.toUpperCase == s2.toUpperCase)
+
+/*  val b1: String = null
+  val b2: String = null
+  println(b1.toUpperCase == b2.toUpperCase)*/
+
+  val c = "Marisa"
+  val d = "marisa"
+  println(c.equalsIgnoreCase(d))
 }
