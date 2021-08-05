7942ab9b7251c055f557daada4ff232cbaffc418
alexandru.castravet <kacmpa@gmail.com>
2021-08-05 23:08:26 +0300

    added all code from capitol 1.2

--- src/main/scala/chapter_1/string/CreatingMultilineStrings.scala
+++ src/main/scala/chapter_1/string/CreatingMultilineStrings.scala
@@ -1,5 +1,35 @@
 package chapter_1.string
 
 object CreatingMultilineStrings extends App {
+  val foo = """This is
+  a multiline
+  String"""
+  println(foo)
+
+  val foo1 = """This is
+  a multiline
+  String"""
+  println(foo1)
+
+  val speech = """Four score and
+  |seven years ago""".stripMargin
+  println(speech)
+
+  val speech1 = """Four score and
+  #seven years ago""".stripMargin('#')
+  println(speech1)
+
+  val speech2 = """Four score and
+  |seven years ago
+  |our fathers""".stripMargin.replaceAll("\n", " ")
+  println(speech2)
+
+  val s = """This is known as a
+  |"multiline" string
+  |or 'heredoc' syntax.""". stripMargin.replaceAll("\n", " ")
+  println(s)
+
+
+
 
 }
