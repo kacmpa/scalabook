6e5429977ae6d2ffa560049ea51ec4ba3fcd08cb
alexandru.castravet <kacmpa@gmail.com>
2021-08-11 23:39:51 +0300

    add exemples with descriptions

--- src/main/scala/chapter_1/string/CreatingMultilineStrings.scala
+++ src/main/scala/chapter_1/string/CreatingMultilineStrings.scala
@@ -2,21 +2,16 @@ package chapter_1.string
 
 object CreatingMultilineStrings extends App {
   val foo = """This is
-  a multiline
-  String"""
+  |a multiline
+  String""".stripMargin
   println(foo)
 
-  val foo1 = """This is
-  a multiline
-  String"""
-  println(foo1)
-
   val speech = """Four score and
-  |seven years ago""".stripMargin
+  |seven years | ago""".stripMargin
   println(speech)
-
+//# lasa spatiu gol inaintea stringului
   val speech1 = """Four score and
-  #seven years ago""".stripMargin('#')
+  # seven years ago""".stripMargin('#')
   println(speech1)
 
   val speech2 = """Four score and
@@ -25,8 +20,8 @@ object CreatingMultilineStrings extends App {
   println(speech2)
 
   val s = """This is known as a
-  |"multiline" string
-  |or 'heredoc' syntax.""". stripMargin.replaceAll("\n", " ")
+  | "multiline" string
+  | or 'heredoc' syntax.""". stripMargin.replaceAll("\n", " ")
   println(s)
 
 
